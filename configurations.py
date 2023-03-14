8f04e66b399198f9849d0e0ab1044df9bc0027a6
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-03-14 08:48:51 +0100

    Create configurations.py

---
+++ configurations.py
@@ -0,0 +1,10 @@
+class Configurations:
+    def __init__(self):
+        self.modelName = "example_model"
+        self.targetNamespace = "http://www.example.com/dfdl"
+        self.fieldNamingConvention = "camelCase"
+        self.maxOccursUnbounded = 1000
+        self.dataTypeOverride = None
+        self.complexTypeName = None
+        self.rootElementName = None
+        self.namespacePrefix = "ex"
